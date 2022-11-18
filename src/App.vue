8757fdf4ab4a7d528e6da06fb0c7eee2bad1771c
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2022-11-18 18:32:27 +0100

    :sparkles: introduce Card component and app

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,50 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import Card from "./components/Card.vue";
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <Card title="Mon titre" />
+
+
+  <!-- MAIN  -->
+
+
+
+  <div class="w-container flex">
+      <div class="flex gap-7 justify-center px-20 flex-col">
+
+          <div class="flex ">
+            <h1 class="">Les plus populaires 🔥</h1>
+            <div class="flex">
+              <img class="h-hDirection w-wDirection" src="img/left.png">
+              <img class="h-hDirection w-wDirection" src="img/right.png">
+            </div>
+          </div>
+          <div class="flex gap-7 justify-center">
+            <Card class="w-wCard h-hCard" img="img/intersect.png" title="management & leadership"
+                  subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="+++" />
+            <Card class="w-wCard h-hCard" img="img/intersect2.png" title="management & leadership"
+                  subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="+" />
+            <Card class="w-wCard h-hCard" img="img/intersect3.png" title="management & leadership"
+                  subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="++" />
+            <Card class="w-wCard h-hCard" img="img/intersect4.png" title="management & leadership"
+                  subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="+++" />
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+  <!-- MAIN  -->
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,27 @@
 <!-- Voir https://vuejs.org/guide/components/props.html#prop-passing-details -->
 
 <script setup>
-defineProps(['title'])
+defineProps(['img', 'title', 'subTitle', 'emoji', 'val'])
 </script>
 
 <template>
-  Mon titre: {{ title }}
+
+  <div class="flex flex-col bg-white-0 shadow-shadowCard rounded-r-radiusCard rounded-l-radiusCard ">
+
+    <img class="w-imgCard" :src="img">
+
+    <div class="flex flex-col items-start px-6 mt-4">
+      <p class="text-xs font-normal uppercase font-Poppins not-italic ">{{title}}</p>
+      <div class="flex flex-col w-contentCard gap-y-6">
+        <h1 class="text-base mt-1 font-Poppins font-semibold">{{subTitle}}</h1>
+        <div class="flex gap-2">
+          <div  v-for="src in val">
+            <img class="w-icon" :src="emoji" >
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
 </template>
