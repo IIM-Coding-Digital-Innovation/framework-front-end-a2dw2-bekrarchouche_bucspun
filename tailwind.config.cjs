01d725f100080ff52684d9b74890b10332db5677
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2022-11-18 12:05:32 +0100

    :wrench: Update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontSize:{
+      xs: ['10px', '18px'],
+      sm: ['12px', '18px'],
+      footer: ['14px', '22px'],
+      base: ['16px', '22px'],
+      lg: ['30px', '34px'],
+      xl: ['60px', '66px'],
+    },
+    colors:{
+      white: ['#FFFFFF'],
+      purple: ['#8D4ED8'],
+      red: ['#FF6A60'],
+      lpurple: ['#F3EDFB'],
+      noir: ['#0A0909'],
+
+    }
   },
   plugins: [],
 }
