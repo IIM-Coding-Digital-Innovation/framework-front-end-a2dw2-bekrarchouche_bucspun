b498f451f1cba32cb27b805b62ae9e5ef3da8ebd
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2022-11-19 18:30:40 +0100

    :bento: Adding components 'buttons'

---
+++ src/components/Button1.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(['text'])
+</script>
+
+<template>
+  <button class="bg-purple-0 text-white-0 rounded-t-radiusButton rounded-b-radiusButton h-hButton px-4
+   font-semibold font-Poppins text-base">{{text}}</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Button2.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(['text'])
+</script>
+
+<template>
+
+  <button class="text-white-0 font-semibold font-Poppins py-3 px-4 bg-purple-0 rounded-b-radiusButton  rounded-t-radiusButton">{{text}}</button>
+</template>
+
+<style scoped>
+
+</style>
