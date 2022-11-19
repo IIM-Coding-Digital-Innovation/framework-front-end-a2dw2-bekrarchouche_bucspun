9d1bba0de2faeede5f46397b401862a38ac48d8f
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2022-11-19 18:33:36 +0100

    :bento: adding cta and pagination (feature pagination incoming)

---
+++ src/components/Cta.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(['text', 'text2'])
+</script>
+
+<template>
+
+  <div class="justify-center">
+    <h3 class="font-bold text-lg font-Poppins text-noir-0">{{text}}</h3>
+    <p class="font-normal text-base font-Poppins text-noir-0">{{text2}}</p>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+const item = document.querySelectorAll("#item");
+
+item.forEach(li => {
+  li.onclick = function () {
+
+    item.forEach(li => {
+      li.id = "item";
+    })
+    li.className = "active";
+  }
+})
+
+</script>
+
+<template>
+
+<ul class="flex gap-6 text-base font-semibold text-grey-0">
+  <li id="item">1</li>
+  <li id="item">2</li>
+  <li id="item">3</li>
+  <li id="item">4</li>
+  <li>...</li>
+  <li id="item">8</li>
+
+</ul>
+
+</template>
+
+<style scoped>
+
+
+
+
+</style>
