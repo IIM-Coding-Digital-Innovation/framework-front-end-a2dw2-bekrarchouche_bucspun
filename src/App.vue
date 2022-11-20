48cab1456764a84ea19ae451a2af46805eed9281
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2022-11-20 17:14:57 +0100

    :art: improve structure

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ import Pagination from "./components/Pagination.vue";
               <img class="h-hDirection w-wDirection" src="img/right.png">
             </div>
           </div>
-          <div class="flex gap-7 justify-center">
+          <div class="flex gap-7 justify-center max-md:flex-wrap">
             <Card class="w-wCard h-hCard" img="img/intersect.png" title="management & leadership"
                   subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="+++" />
             <Card class="w-wCard h-hCard" img="img/intersect2.png" title="vente"
@@ -47,7 +47,7 @@ import Pagination from "./components/Pagination.vue";
       <div class="flex ">
         <h1 class="uppercase mt-24">toute les formations 🚀</h1>
       </div>
-      <div class="flex gap-7 justify-center">
+      <div class="flex gap-7 justify-center max-md:flex-wrap">
         <Card class="w-wCard h-hCard" img="img/intersect5.png" title="thématique 1"
               subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="+++" />
         <Card class="w-wCard h-hCard" img="img/intersect6.png" title="thématique 1"
@@ -64,7 +64,7 @@ import Pagination from "./components/Pagination.vue";
   <!--  CARD-ROW 3  -->
   <div class="flex justify-center mt-10">
     <div class="flex gap-7 justify-center flex-col px-20">
-      <div class="flex gap-7 justify-center">
+      <div class="flex gap-7 justify-center max-md:flex-wrap">
         <Card class="w-wCard h-hCard" img="img/intersect9.png" title="thématique 1"
               subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="++" />
         <Card class="w-wCard h-hCard" img="img/intersect10.png" title="thématique 1"
@@ -81,7 +81,7 @@ import Pagination from "./components/Pagination.vue";
   <!--  CARD-ROW 4  -->
   <div class="flex justify-center mt-10">
     <div class="flex gap-7 justify-center flex-col px-20">
-      <div class="flex gap-7 justify-center">
+      <div class="flex gap-7 justify-center max-md:flex-wrap">
         <Card class="w-wCard h-hCard" img="img/intersect13.png" title="thématique 1"
               subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="++" />
         <Card class="w-wCard h-hCard" img="img/intersect14.png" title="thématique 1"
@@ -91,7 +91,7 @@ import Pagination from "./components/Pagination.vue";
         <Card class="w-wCard h-hCard" img="img/intersect16.png" title="thématique 1"
               subTitle="Accueillir un.e collègue en situation de handicap" emoji="img/emoji.png" val="+" />
       </div>
-      <Pagination class="flex justify-end mt-5"/>
+      <Pagination class="flex justify-end mt-5 max-md:justify-center"/>
     </div>
   </div>
 
@@ -101,18 +101,14 @@ import Pagination from "./components/Pagination.vue";
 
   <!--  BOTTOM WINDOW  -->
 
-  <div class="flex justify-center mb-16  mt-24 ">
-    <div class="flex bg-lpurple-0 justify-center text-center flex-col w-wBoxCta h-hBoxCta rounded-r-radiusCard rounded-l-radiusCard">
-
-      <Cta text="C'est votre dernier mot ?" text2="Si vous n'avez pas trouvé ce que vous, dites-le nous." />
-
-      <div class="mt-6">
-        <Button1 class="mr-6" text="Je prends contact"/>
-        <Button2 text="Télecharger le catalogue" />
-      </div>
-
+  <div class="flex justify-center mb-16  mt-24  max-md:px-4">
+    <div class="max-md:pt-8 max-md:pb-6 py-3.5 flex bg-lpurple-0 justify-center text-center flex-col w-wBoxCta h-hBoxCta rounded-r-radiusCard rounded-l-radiusCard max-md:h-h-r-BoxCta">
+      <Cta class="max-md:mt-12" text="C'est votre dernier mot ?" text2="Si vous n'avez pas trouvé ce que vous cherchez, dites-le nous." />
+        <div class="flex md:space-x-6 mt-6 px-6 justify-center max-md:flex-col max-md:space-y-4 max-md:mb-12">
+          <Button1 class="px-6" text="Je prends contact"/>
+          <Button2 text="Télecharger le catalogue" />
+        </div>
     </div>
-
   </div>
 
 
